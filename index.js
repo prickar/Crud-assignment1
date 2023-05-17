5fb35311dd5d1cf80acce25f09d831f5c69df041
Paola <paola.backstrom@gmail.com>
2023-05-17 14:38:05 +0200

    prettier

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const movies = require('./routes/movies');
+const express = require("express");
+const movies = require("./routes/movies");
 
-const app = express(); 
-const port = 3009; 
+const app = express();
+const port = 3009;
 
 app.use(express.json());
 
 const validApiKey = ["5", "10"];
 
 const authenticateApiKey = (req, res, next) => {
-  const apiKey = req.query.apiKey;
+	const apiKey = req.query.apiKey;
 
-  if (!apiKey) {
-    return res
-      .status(401)
-      .json({ message: "No movie for you, your apikey is missing" });
-  }
+	if (!apiKey) {
+		return res
+			.status(401)
+			.json({ message: "No movie for you, your apikey is missing" });
+	}
 
-  if (apiKey !== validApiKey) {
-    return res
-    .status(403)
-    .json({ message: "Invalid API key" });
-  }
+	if (apiKey !== validApiKey) {
+		return res.status(403).json({ message: "Invalid API key" });
+	}
 
-  next();
+	next();
 };
 
 app.use((req, res, next) => {
-  authenticateApiKey(req, res, next);
+	authenticateApiKey(req, res, next);
 });
 
-app.get('/', (req, res) => {
-    res.send('Im Bond, James Bond')
+app.get("/", (req, res) => {
+	res.send("Im Bond, James Bond");
 });
 
-app.use('/movies', movies)
+app.use("/movies", movies);
 
 //portserver
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`);
 });
--- routes/movies.js
+++ routes/movies.js
@@ -1,113 +1,112 @@
-const express = require('express');
-const mockData = require('../mockData');
+const express = require("express");
+const mockData = require("../mockData");
 const router = express.Router();
 
-let movies = mockData; 
-
+let movies = mockData;
 
 //find all movies GET
-router.get('/', (req, res) => {
-  res.json(movies);
+router.get("/", (req, res) => {
+	res.json(movies);
 });
 
 //find a movie based on their Id GET
-router.get('/:id', (req, res) => {
-  const imdbID = req.params.id;
-  const movie = movies.find((film) => film.imdbID === imdbID);
+router.get("/:id", (req, res) => {
+	const imdbID = req.params.id;
+	const movie = movies.find((film) => film.imdbID === imdbID);
 
-  if (!movie) {
-    return res.status(404).json({
-      message: 'No movie with this ID exists!',
-    });
-  }
+	if (!movie) {
+		return res.status(404).json({
+			message: "No movie with this ID exists!",
+		});
+	}
 
-  res.json(movie);
+	res.json(movie);
 });
 
-//creates a new Id for new added movies 
+//creates a new Id for new added movies
 
-let nextId = 2211; 
+let nextId = 2211;
 
-//Add a new movie POST 
-router.post('/', (req, res) => {
-    const movie = req.body.movie; 
+//Add a new movie POST
+router.post("/", (req, res) => {
+	const movie = req.body.movie;
 
-    if (!movie.Title || !movie.Year || !movie.Released || !movie.Genre) {
-      return res 
-      .status(400)
-      .json({message: "You need to add title, year, release date and genre"});
-    }
+	if (!movie.Title || !movie.Year || !movie.Released || !movie.Genre) {
+		return res
+			.status(400)
+			.json({
+				message: "You need to add title, year, release date and genre",
+			});
+	}
 
-    if (isNaN(movie.Year) || isNaN(movie.Released)) {
-      return res
-      .status(400)
-      .json({message: "The inputs have to be numbers"});
-    }
+	if (isNaN(movie.Year) || isNaN(movie.Released)) {
+		return res
+			.status(400)
+			.json({ message: "The inputs have to be numbers" });
+	}
 
-    const addMovie = {
-        ...movie, 
-        imdbID: nextId, 
-    }; 
+	const addMovie = {
+		...movie,
+		imdbID: nextId,
+	};
 
-    nextId++;
+	nextId++;
 
-    movies.push(addMovie);
-    res.json(addMovie);
+	movies.push(addMovie);
+	res.json(addMovie);
 });
 
 //Update a movie based on their Id
 
 router.put("/:id", (req, res) => {
-  const imdbID = req.params.id
-  const IDnumber = imdbID;
-  const movie = req.body.movie; 
+	const imdbID = req.params.id;
+	const IDnumber = imdbID;
+	const movie = req.body.movie;
 
-  const index = movies.findIndex((film) => film.imdbID === IDnumber);
+	const index = movies.findIndex((film) => film.imdbID === IDnumber);
 
-  if (!movie.Title || !movie.Year || !movie.Released || !movie.Genre) {
-    return res 
-    .status(400)
-    .json({message: "You need to add title, year, release date and genre"});
-  }
+	if (!movie.Title || !movie.Year || !movie.Released || !movie.Genre) {
+		return res
+			.status(400)
+			.json({
+				message: "You need to add title, year, release date and genre",
+			});
+	}
 
-  if (isNaN(movie.Year) || isNaN(movie.Released)) {
-    return res
-    .status(400)
-    .json({message: "The inputs have to be numbers"});
-  }
+	if (isNaN(movie.Year) || isNaN(movie.Released)) {
+		return res
+			.status(400)
+			.json({ message: "The inputs have to be numbers" });
+	}
 
-  if (index === -1) {
-    return res
-    .status(404)
-    .json({ mesage: " No id match any existing movie"});
-  }
+	if (index === -1) {
+		return res
+			.status(404)
+			.json({ mesage: " No id match any existing movie" });
+	}
 
-  const updatedmovie = { ...movies[index], ...movie }; 
-  movies[index] = updatedmovie; 
+	const updatedmovie = { ...movies[index], ...movie };
+	movies[index] = updatedmovie;
 
-  res.json(updatedmovie);
-})
+	res.json(updatedmovie);
+});
 
-
-//Delete a movie from the list 
+//Delete a movie from the list
 
 router.delete("/:id", (req, res) => {
-  const imdbID = req.params.id; 
-  const IDnumber = imdbID;
+	const imdbID = req.params.id;
+	const IDnumber = imdbID;
 
-  const movie = movies.find((film) => film.imdbID === IDnumber);
+	const movie = movies.find((film) => film.imdbID === IDnumber);
 
-  if (!movie) {
-    return res
-    .status(404)
-    .json({message: "No movie with this Id exist"});
-  }
+	if (!movie) {
+		return res.status(404).json({ message: "No movie with this Id exist" });
+	}
 
-  const newData = movies.filter((film) => film.imdbID !== IDnumber);
-  movies = newData; 
+	const newData = movies.filter((film) => film.imdbID !== IDnumber);
+	movies = newData;
 
-  res.json({message: "The movie ' " + movie.Title + "' have been deleted"});
-}); 
-
+	res.json({ message: "The movie ' " + movie.Title + "' have been deleted" });
+});
 
 module.exports = router;
